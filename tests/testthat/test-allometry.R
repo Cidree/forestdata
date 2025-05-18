68909c5a105223af3b25a7cefd3c02943f58e7a7
Cidree <adrian.cidre@gmail.com>
2025-05-18 18:09:54 +0100

    edit allometry test

--- tests/testthat/test-allometry.R
+++ tests/testthat/test-allometry.R
@@ -3,18 +3,15 @@
 
 ## Get data
 tallo_tbl <- fd_allometry_tallo()
-tallo_sf  <- fd_allometry_tallo(spatial = TRUE, quiet = TRUE)
 
 ## Check formats
 test_that("Database is downloaded in correct format", {
   expect_equal(class(tallo_tbl)[1], "tbl_df")
-  expect_equal(class(tallo_sf)[1], "sf")
 })
 
 ## Data is downloaded
 test_that("Data is downloaded", {
   expect_gt(nrow(tallo_tbl), 1)
-  expect_gt(nrow(tallo_sf), 1)
 })
 
 
