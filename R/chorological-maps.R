55b8c35ea38b90d08b4b804ac9f75b2522da5411
Cidree <adrian.cidre@gmail.com>
2025-07-24 21:56:48 +0100

    Use API to easily access the latest shapefile

--- R/chorological-maps.R
+++ R/chorological-maps.R
@@ -46,99 +46,62 @@
 #'  }
 fd_forest_chorological <- function(species, range = "nat", quiet = FALSE) {
 
-  # 1. Get species table
-  choro_tbl <- chorological_tbl
+  # 1. Request url
+  ## 1.1. Species code
+  species_code <- chorological_tbl$code[chorological_tbl$Species == species]
+  ## 1.2. Url
+  url <- paste0("https://api.figshare.com/v2/articles/", species_code)
+  ## 1.3. Get article metadata
+  req <- httr2::request(url) |>
+    httr2::req_user_agent("R httr2 client") |>
+    httr2::req_perform()
+  ## 1.4. Get metadata
+  resp_json <- httr2::resp_body_json(req)
 
-  # 2. Filter user species name
-  ## 2.1. Stop if species is incorrect
-  if (!species %in% choro_tbl$Species) cli::cli_abort("The species name is not supported.")
-  ## 2.2. Filter species
-  user_species_tbl <- choro_tbl |>
-    dplyr::filter(Species == species)
-
-  # 3. Download the species to tempdir
-  ## 3.1. Create download links
-  download_url       <- stringr::str_glue("https://figshare.com/ndownloader/articles/{user_species_tbl$code}/versions")
-  user_species_zip   <- stringr::str_glue("{tempdir()}/{user_species_tbl$code}.zip")
-  user_species_unzip <- stringr::str_glue("{tempdir()}/{user_species_tbl$code}")
-  if (file.exists(user_species_zip)) file.remove(user_species_zip)
-  if (file.exists(user_species_unzip)) unlink(user_species_unzip, recursive = TRUE, force = TRUE)
-  if (!quiet) cli::cli_progress_step("Downloading data...", msg_done = "Downloaded", "Download failed")
-  ## 3.2. Download version 5
-  file.d <- fdi_download(
-    download_url = stringr::str_glue("{download_url}/5"),
-    destfile     = user_species_zip
+  # 2. Get download url
+  ## 2.1. Table with downloadable files
+  files_info <- data.frame(
+    name = sapply(resp_json$files, function(x) x$name),
+    download_url = sapply(resp_json$files, function(x) x$download_url),
+    type = sapply(resp_json$files, function(x) x$mimetype)
   )
-  ## 3.3. Handle when version 5 is not available
-  ## Download version 4
-  if (!file.d) {
-    file.d <- fdi_download(
-      download_url = stringr::str_glue("{download_url}/4"),
-      destfile     = user_species_zip
-    )
-  }
-  ### Version 3
-  if (!file.d) {
-    file.d <- fdi_download(
-      download_url = stringr::str_glue("{download_url}/3"),
-      destfile     = user_species_zip
-    )
-  }
-  ### Version 2
-  if (!file.d) {
-    file.d <- fdi_download(
-      download_url = stringr::str_glue("{download_url}/2"),
-      destfile     = user_species_zip
-    )
-  }
-  ### Version 1
-  if (!file.d) {
-    file.d <- fdi_download(
-      download_url = stringr::str_glue("{download_url}/1"),
-      destfile     = user_species_zip
-    )
-  }
-  ## Check for success
+  ## 2.2. Filter the zip folder
+  zip_info <- files_info[files_info$type == "application/zip", ]
+  zip_info <- files_info[grep(".zip", files_info$name), ]
+
+  # 3. Download to tempdir
+  ## 3.1. Download
+  dir_zip   <- paste0(tempdir(), "/", zip_info$name)
+  dir_unzip <- paste0(tempdir(), "/", basename(zip_info$download_url))
+  if (!quiet) cli::cli_progress_step("Downloading data...", msg_done = "Downloaded", "Download failed")
+  file.d <- fdi_download(zip_info$download_url, destfile = dir_zip)
+  ## 3.2. Check for success
   if (!file.d) {
     cli::cli_process_failed()
     return(cli::cli_alert_danger("`fd_forest_chorological()` failed to retrieve the data. Service might be currently unavailable"))
   }
-    ## 3.4. Unzip
-  unzip(
-    zipfile = user_species_zip,
-    exdir   = user_species_unzip
-  )
-
   # 4. Get the shapefile
-  ## 4.1. Create paths
-  species_shp <- stringr::str_glue("{user_species_tbl$Species |> stringr::str_replace_all(' ', '_')}_shp") |>
-    stringr::str_replace_all("-", "")
-  user_species_shp_zip   <- stringr::str_glue("{user_species_unzip}/{species_shp}.zip")
-  user_species_shp_unzip <- stringr::str_glue("{user_species_unzip}/{species_shp}")
-  ## 4.2. Unzip the shapefile
-  unzip(
-    zipfile = user_species_shp_zip,
-    exdir   = user_species_shp_unzip
-  )
+  ## 4.1. Unzip
+  unzip(dir_zip, exdir = dir_unzip, overwrite = TRUE)
   ## 4.2. Check for range
   if (range == "nat") {
     ## check files which contain "plg" and end with ".shp
-    files <- list.files(user_species_shp_unzip, pattern = "plg.*\\.shp$")
+    files <- list.files(dir_unzip, pattern = "plg.*\\.shp$")
     ## eliminate files containing "syn
     files_filtered <- files[!grepl("syn|pnt", files)]
     ## sort decreasing and take first one (the one with clip if exists)
     selected_file <- sort(files_filtered, decreasing = TRUE)[1]
     if (is.na(selected_file)) cli::cli_abort("There's no range = `{range}` for {species}")
-    path_shp <- stringr::str_glue("{user_species_shp_unzip}/{selected_file}")
+    path_shp <- paste0(dir_unzip, "/", selected_file)
   } else if (range == "syn") {
     ## check files which contain "syn" and end with ".shp
-    files <- list.files(user_species_shp_unzip, pattern = "syn.*\\.shp$")
-    ## eliminate files containing "syn
+    files <- list.files(dir_unzip, pattern = "syn.*\\.shp$")
+    ## eliminate files containing pnt
     files_filtered <- files[!grepl("pnt", files)]
     ## sort decreasing and take first one (the one with clip if exists)
     selected_file <- sort(files_filtered, decreasing = TRUE)[1]
     if (is.na(selected_file)) cli::cli_abort("There's no range = `{range}` for {species}")
-    path_shp <- stringr::str_glue("{user_species_shp_unzip}/{selected_file}")
+    path_shp <- paste0(dir_unzip, "/", selected_file)
   }
   ## 4.3. Read file
   if (!quiet) cli::cli_process_done()
@@ -146,13 +109,4 @@ fd_forest_chorological <- function(species, range = "nat", quiet = FALSE) {
   sf::read_sf(path_shp)
 
 }
-
-
-
-
-
-
-
-
-
 
